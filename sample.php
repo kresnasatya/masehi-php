5de8dc266e7609b56f3bd7619c066fd83c0a2622
Satya Kresna <satyakresna6295@gmail.com>
2018-12-02 12:21:30 +0800

    Rename index.php to sample.php

--- sample.php
+++ sample.php
@@ -3,27 +3,27 @@ require 'vendor/autoload.php';
 
 use Masehi\Conveter as Converter;
 
-echo Masehi\Converter::convert(array(
+echo Masehi\Converter::convertDate(array(
   "date" => "2013/08/25",
   "format" => "d M Y",
 ));
 echo "\n";
-echo Masehi\Converter::convert(array(
+echo Masehi\Converter::convertDate(array(
   "date" => "2013/08/25",
   "format" => "d F Y",
 ));
 echo "\n";
-echo Masehi\Converter::convert(array(
+echo Masehi\Converter::convertDate(array(
   "date" => "2013/08/25",
   "format" => "D, d F Y",
 ));
 echo "\n";
-echo Masehi\Converter::convert(array(
+echo Masehi\Converter::convertDate(array(
   "date" => "2013/08/25",
   "format" => "l, d F Y",
 ));
 echo "\n";
-echo Masehi\Converter::convert(array(
+echo Masehi\Converter::convertDate(array(
   "date" => "2013/08/25",
   "format" => "l, d M Y",
 ));
